8ef411cd795f0e5500797f47e43f3fd30387936b
bsteve <bicosteve4@gmail.com>
2025-05-27 20:30:45 +0300

    added test for access nested map exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,6 +18,22 @@ class TestAccessNestedMap(unittest.TestCase):
     )
     def test_access_nested_map(self, nested_map, path, expected):
         """
-        This method tests the access nested map and asserts if the nested map and path params are equal to expected.
+        This method tests the access nested map and asserts if the nested
+        map and path params are equal to expected.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand(
+        [
+            ({}, ("a",), "a"),
+            ({"a": 1}, ("a", "b"), "b"),
+        ]
+    )
+    def test_access_nested_map_exception(self, nested_map, path, expected_key):
+        """
+        This method tests the access nested map and asserts if the nested
+        map and path params and raises keyerror indicating the wrong key
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{expected_key}'")
