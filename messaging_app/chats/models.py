d86a651810a0cc6ff9cb057c9310e67af2ca6aa8
bsteve <bicosteve4@gmail.com>
2025-06-01 10:40:05 +0300

    fixed models issues

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,17 +1,26 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# Create your models here.
 class User(AbstractUser):
-    bio = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to="profiles/", blank=True, null=True)
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True, blank=False, null=False)
+    phone_number = models.CharField(max_length=20, blank=False, null=False)
+    first_name = models.CharField(max_length=20, blank=False, null=False)
+    last_name = models.CharField(max_length=20, blank=False, null=False)
+    password = models.CharField(max_length=20, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.username
+        return self.email
 
 
 class Conversation(models.Model):
+    conversation_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
     participants = models.ManyToManyField("User", related_name="converstions")
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -20,15 +29,16 @@ class Conversation(models.Model):
 
 
 class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     sender = models.ForeignKey(
         "User", on_delete=models.CASCADE, related_name="messages"
     )
     conversation = models.ForeignKey(
         "Conversation", on_delete=models.CASCADE, related_name="messages"
     )
-    text = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    message_ody = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"From {self.sender.username} at {self.timestamp}"
+        return f"From {self.sender.username} at {self.sent_at}"
