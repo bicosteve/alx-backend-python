426522d9df3f1c71c12350116fca1b3e490f7e4d
bsteve <bicosteve4@gmail.com>
2025-06-01 10:52:32 +0300

    fixed serializer errors

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,13 +1,37 @@
 from rest_framework import serializers
 
 
-from chats.models import User, Conversation, Message
+from .models import User, Conversation, Message
 
 
-class UserSerializer(serializers.Serializer):
+class UserSerializer(serializers.ModelSerializer):
+    user_id = serializers.UUIDField(read_only=True)
+    email = serializers.CharField(blank=False, null=False)
+    phone_number = serializers.CharField(max_length=20)
+    first_name = serializers.CharField(max_length=20)
+    last_name = serializers.CharField(max_length=20)
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = "__all__"
+
+        read_only_fields = ["user_id"]
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with this email already exists.")
+        return value
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
 
 
 class MessageSerializer(serializers.Serializer):
