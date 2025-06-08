ed71064e74669d7533ff7bcd87e84f03b3c48197
bsteve <bicosteve4@gmail.com>
2025-06-08 08:41:24 +0300

    refactored the app folder

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,56 @@
+from rest_framework import serializers
+
+
+from .models import User, Conversation, Message
+
+
+class UserSerializer(serializers.ModelSerializer):
+    user_id = serializers.UUIDField(read_only=True)
+    email = serializers.CharField(blank=False, null=False)
+    phone_number = serializers.CharField(max_length=20)
+    first_name = serializers.CharField(max_length=20)
+    last_name = serializers.CharField(max_length=20)
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = "__all__"
+
+        read_only_fields = ["user_id"]
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with this email already exists.")
+        return value
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class MessageSerializer(serializers.Serializer):
+    sender = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = "__all__"
+
+
+class ConversationSerialzer(serializers.Serializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    message_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Conversation
+        fields = "__all__"
+
+    def get_message_count(self, object):
+        return object.messages.count()
---
+++ messaging_app/chats/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework_nested.routers import NestedDefaultRouter
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+from .views import ConversationViewSet, MessageViewSet
+
+router = routers.DefaultRouter()
+router.register(r"conversations", ConversationViewSet, basename="conversation")
+router.register(r"messages", MessageViewSet, basename="message")
+
+urlpatterns = [
+    path("/api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("/api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("/api", include(router.urls)),
+]
