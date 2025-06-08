9f11bfa2864da4616b1290a91b65d60b90241915
bsteve <bicosteve4@gmail.com>
2025-06-08 09:39:22 +0300

    added pagination count

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -5,3 +5,11 @@ class MessagePagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = "page_size"
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return {
+            "count": self.page.paginator.count,
+            "result": data,
+            "next": self.get_next_link(),
+            "previous": self.get_previous_link(),
+        }
