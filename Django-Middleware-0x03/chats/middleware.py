3968efae4ccd14b4fa531a9708c157cebb7e62a6
bsteve <bicosteve4@gmail.com>
2025-06-08 11:34:21 +0300

    fixed the middleware issue

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -7,7 +7,17 @@ logging.basicConfig(filename="requests.log", level=logging.INFO, format="%(messa
 
 
 class RequestLoggingMiddleware(MiddlewareMixin):
-    def process_request(self, request):
-        user = request.user if request.user.is_authenticated else "Not know"
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user = request.user if request.user.is_authenticated else "Not known"
         log_entry = f"{datetime.datetime.now()} - User: {user} - Path: {request.path}"
         logging.info(log_entry)
+        response = self.get_response(request)
+        return response
+
+    # def process_request(self, request):
+    #     user = request.user if request.user.is_authenticated else "Not know"
+    #     log_entry = f"{datetime.datetime.now()} - User: {user} - Path: {request.path}"
+    #     logging.info(log_entry)
